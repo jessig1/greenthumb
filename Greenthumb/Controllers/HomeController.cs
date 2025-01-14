8d51c0c8a94f6ff72f543813d3d65b36e10e7d63
Jake <essigj88@outlook.com>
2025-01-13 21:30:57 -0500

    added about page

--- Greenthumb/Controllers/HomeController.cs
+++ Greenthumb/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Greenthumb.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Greenthumb.Controllers
@@ -9,12 +10,26 @@ namespace Greenthumb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Data.GreenthumbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Data.GreenthumbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        public async Task<ActionResult> About()
+        {
+            IQueryable<Models.GreenthumbViewModels.EnrollmentDateGroup> data =
+                from user in _context.Users
+                group user by user.SignupDate into dateGroup
+                select new Models.GreenthumbViewModels.EnrollmentDateGroup()
+                {
+                    SignupDate = dateGroup.Key,
+                    UserCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
         public IActionResult Index()
         {
             return View();
---
+++ Greenthumb/Models/GreenthumbViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Greenthumb.Models.GreenthumbViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? SignupDate { get; set; }
+
+        public int UserCount { get; set; }
+
+    }
+}
